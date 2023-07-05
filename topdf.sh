2ac8215d9b771862a4920a5b0c94183606a4f86c
vigvega <victoriagv01@gmail.com>
2023-07-05 13:28:28 +0200

    pdf utility

---
+++ topdf.sh
@@ -0,0 +1,26 @@
+#############################
+#		PDF CONVERTER		#
+#############################
+
+#! /bin/bash
+
+pacg=$(which ghostscript)
+pacl=$(which lowriter)
+
+if [ "$pacg" = "" ] || [ "$pacl" = "" ]
+then
+	echo "Error. Try: sudo apt-get install libreoffice && sudo apt-get install ghostscript"
+else
+	ls
+	read -p "File to convert:" file
+	# Compruebo si existe el fichero
+	if [ -f "$file" ]
+	then
+		echo -e "\e[0;32mProcessing...\e[0m"
+		# Convierto el archivo a pdf
+		lowriter --convert-to pdf $file
+		echo -e "\e[0;32mDone\e[0m"
+	else
+		echo "File not found :("
+	fi
+fi
